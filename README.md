31947e4da209aa1724c4cae1cb71a9c244603e08
DEVBAHAR\shuky_000 <support@billiving.com>
2018-11-05 12:35:57 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# toggl Connector
+
+[apigum.com](https://www.apigum.com) utilizes the [toggl](https://www.toggl.com/) connector for building integrations with other popular cloud applications. We would love to get your code contribution for existing applications or for new ones you'd like to be using.
+
+ For available toggl integrations please visit this [page](https://www.apigum.com/apps/toggl).
+
+
+ 
---
+++ azure/index.js
@@ -0,0 +1,37 @@
+var request = require("request");
+
+
+module.exports = function(context, req) {
+    
+        context.log('starting..');
+        let auth = "Basic " + new Buffer(req.body.api_token + ":api_token").toString("base64");
+        
+        var obj = {
+            method: req.body.httpverb,
+            uri: req.body.url,
+            json: true,
+            headers: {
+                "Authorization": auth
+            }
+        };
+
+        if (req.body.payload) obj.body = (typeof req.body.payload=='string') ? JSON.parse(req.body.payload) : req.body.payload;
+
+        new Promise((resolve, reject) => {
+            request(
+                obj,
+                function(error, response, body) {
+                    let message = `Call of ${req.body.url}`;
+                    if (error) {
+                        context.log(message + " failed", error);
+                        reject(error);
+                        return;
+                    } else {
+                        context.res = { status: response.statusCode, body: body };
+                        context.done();
+                    }
+                }
+            );
+        })
+    
+};
